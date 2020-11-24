c5239f36b3f0c90e0c2b0b4928d152dda29fe5eb
Klmpk11 <74640830+Klmpk11@users.noreply.github.com>
2020-11-24 15:04:01 +0700

    Update destruktor.php

--- destruktor.php
+++ destruktor.php
@@ -1,25 +1,26 @@
 <?php
- //class buah
- class buah (
-   //property 
+ class buah {
+   
    var $buah; 
    var $warna; 
-   //method construct dijalankan pertama kali
+ 
    function_construct (){
      echo "ini adalah isi method construct </br>"
      }
-   //method destruct dijalankan terakhir
+  
    function_destruct (){
      echo "ini adalah isi method destruct </br>"; 
      
    }
-   //method buah 
+ 
    function tampilkan_buah (){
      return "Saya Adalah Buah Anggur </br> "; 
    }
    
    }
-   //instansiasi class buah 
+
    $buah = new buah (); 
-   //memanggil method tampilkan_nama dari class buah
    echo $buah ">tampilkan_buah();
+
+   ?>
+
