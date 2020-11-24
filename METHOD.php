88f987419925354c3d3719e9bb855fe40107865c
Klmpk11 <74640830+Klmpk11@users.noreply.github.com>
2020-11-24 13:20:17 +0700

    Update METHOD.php

--- METHOD.php
+++ METHOD.php
@@ -1,17 +1,17 @@
 <?php
 //cara penulisan class dan property OOP PHP - @pulung_coding
-class mobil{
+class baju{
     //property OOP
     var $warna;
     var $merek;
     var $ukuran;
     
     //method OOP
-    function maju() {
-    //isi method
+    function jumlah() {
+        echo "Saya membeli tiga baju </br>";
 }
-    function berhenti() {
-        //isi method
+    function warna() {
+        echo "Saya pesan baju berwarna biru </br>";
       }
       
 }
