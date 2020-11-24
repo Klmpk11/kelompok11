8e410a62aef9ae716ebe86b8739dd9572d9d89ee
Klmpk11 <74640830+Klmpk11@users.noreply.github.com>
2020-11-24 15:01:23 +0700

    Update construktor.php

--- construktor.php
+++ construktor.php
@@ -1,22 +1,17 @@
 <?php
-//class buah
 class buah {
-         //property
+        
          var $buah ;
          var $warna ;
 
-         function _construct ()  {
-                  echo  "ini adalah isi method construct </br>";
+         function_construct ()  {
+                  echo  "ini adalah isi method construct <br>";
+         }
+ 
+         function tampilkan_nama () {
+                  return "Aku adalah buah jeruk <br>";
          }
 
-         //method buah 
-         function tampilkan_buah() {
-                  return "Aku adalah buah jeruk </br>";
-         }
-
-}
-//instansiasi class buah 
+} 
 $buah = new buah() ; 
-
-//memanggil method tampilkan nama dari class buah 
-echo $buah -> tampilkan_buah () ;
+echo $buah-> tampilkan_nama () ;
