324d67b53ea97ee59f9fdb8537b47c5e90d2f8f9
Klmpk11 <74640830+Klmpk11@users.noreply.github.com>
2020-11-29 18:48:25 +0700

    Update enkapsiluasiprotecd.php

--- enkapsiluasiprotecd.php
+++ enkapsiluasiprotecd.php
@@ -1,17 +1,24 @@
 <?php
-   class buah{
-     procted $buah = "Jeruk";
-     
-     protected function buah() {
-         return "Saya adalah buah " .$this->nama;
-     }
-     
-     public function tampilkan_nama() {
-         return $this->tampilkan_nama;
-     }
-   }
-   
-   $buah = new buah();
-   echo $buah->tampilkan_nama();
 
- ?>
+//class buah
+class buah{
+  //menentukan property dengan protected   
+  protected $nama = "Jeruk";
+  
+  //method protected
+  protected function nama(){
+    return "Saya adalah buah " .$this->nama;
+  }
+  
+  public function tampilkan_nama(){
+    return $this->tampilkan_nama;
+  }
+  
+}
+ 
+//instansiasi class buah
+ $buah = new buah();
+ 
+//memanggil method public tampilkan_nama dari class buah
+ echo $buah->tampilkan_nama() ;
+?>
