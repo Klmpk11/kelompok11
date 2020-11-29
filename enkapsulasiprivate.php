391190c008f75fec9eee3dda82aea8ec0a1c9852
Klmpk11 <74640830+Klmpk11@users.noreply.github.com>
2020-11-29 17:05:23 +0700

    Update enkapsulasiprivate.php

--- enkapsulasiprivate.php
+++ enkapsulasiprivate.php
@@ -3,14 +3,17 @@
 //class buah
 class buah{
   // menentukan property dengan private
-  private $buah = "Apel";
+  private $nama = "Apel";
   
   // method public
   public function tampilkan_nama(){
-      return "Saya Membeli " .$this->buah;
+      return "Saya Membeli " .$this->nama;
   }
 }
-// memanggil method public tampilkan_nama dari class manusia
-echo $buah->tampilkan_buah();
+//instansiasi class buah
+$buah = new buah();
+
+// memanggil method public tampilkan_nama dari class buah
+echo $buah->tampilkan_nama();
 
 ?>
